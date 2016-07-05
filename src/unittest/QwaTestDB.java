85ca06335d9379d10cc616765967fa1a30c6fd69
talbyte <talbyte@gmail.com>
2016-07-05 16:36:36 +0200

    QuizSet to GSON Test

--- src/unittest/QwaTestDB.java
+++ src/unittest/QwaTestDB.java
@@ -2,31 +2,30 @@ package unittest;
 
 import static org.junit.Assert.*;
 
+import model.Answer;
+import model.Question;
 import model.QuizSet;
 import org.json.JSONObject;
 import org.junit.Test;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 import model.Qwa;
 import persistenz.DBManager;
 
 import javax.xml.ws.Response;
+import java.io.StringWriter;
+import java.util.ArrayList;
+import java.util.List;
 
 public class QwaTestDB {
 
 	@Test
 	public void test() {
+
 		Qwa qwa = DBManager.getQwaByID(1);
-		Qwa qwa2 = DBManager.getQwaByID(2);
 		assertEquals("Was ist Spring?", qwa.getQuestion().getText());
 		assertEquals("Java Webframework", qwa.getAnswers().get(0).getText());
 		assertEquals("Party in München", qwa.getAnswers().get(1).getText());
-/*
-		JSONObject jsonObject = new JSONObject();
-		QuizSet qs = new QuizSet();
-		qs.addQwa(DBManager.getQwaByID(1));
-
-		jsonObject.put("QuizSet", qs);
-
-		System.out.println(jsonObject);*/
 	}
 }
