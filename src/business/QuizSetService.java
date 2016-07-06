63bd99bb314d6447a58f25ce8818105b49998a52
talbyte <talbyte@gmail.com>
2016-07-06 16:14:02 +0200

    Rest Class

--- src/business/QuizSetService.java
+++ src/business/QuizSetService.java
@@ -1,44 +1,61 @@
 package business;
 
+import java.io.StringWriter;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
 
 import org.json.JSONException;
-import org.json.JSONObject;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
+import model.QuizSet;
+
+@Path("/quizsetservice")
 public class QuizSetService {
 
-    @Path("/quizsetservice")
-    public class PoundToEuroConverter {
+	@GET
+    @Produces("application/json")
+	public Response getQuizSetAllAnswers() {
+        Gson gson = new GsonBuilder().create();
+        QuizSet quizSet = QuizUtils.generateRandomQuizSet(9);
+        StringWriter stringWriter = new StringWriter();
+        gson.toJson(quizSet, stringWriter);
+        return Response.status(200).entity(stringWriter.toString()).build();
+	}
+	
+    @Path("{count}")
+    @GET
+    @Produces("application/json")
+    public Response getQuizSetSomeAnswers(@PathParam("count") int count) throws JSONException {
+    	return null;
+    }
+	
+    @POST
+    @Consumes("application/json")
+    @Produces("application/json")
+    public QuizSet create(QuizSet quizSet) {
+        return QuizUtils.create(quizSet);
+    }
 
-        @GET
-        @Produces("application/json")
-        public Response convertPoundToEuro() throws JSONException {
-            JSONObject jsonObject = new JSONObject();
-            Double pound = 1d;
-            Double euro = pound * 1.24671d;
+    @PUT
+    @Path("{id}")
+    @Consumes("application/json")
+    @Produces("application/json")
+    public QuizSet update(QuizSet quizSet) {
+        return QuizUtils.update(quizSet);
+    }
 
-            jsonObject.put("Pound value", pound);
-            jsonObject.put("Euro value", euro);
-
-            return Response.status(200).entity(jsonObject).build();
-        }
-
-        @Path("{p}")
-        @GET
-        @Produces("application/json")
-        public Response convertEuroToPound(@PathParam("p") Double p) throws JSONException {
-            JSONObject jsonObject = new JSONObject();
-            Double euro = 1d;
-            Double pound = euro * 1.24671d;
-
-            jsonObject.put("Pound value", pound);
-            jsonObject.put("Euro value", euro);
-
-            return Response.status(200).entity(jsonObject).build();
-        }
+    @DELETE
+    @Path("{id}")
+    @Produces("application/json")
+    public void remove(@PathParam("id") int id) {
+    	QuizUtils.remove(id);
     }
 }
