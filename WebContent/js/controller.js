84cb9770caeced4844a510d261495caba1ddfab8
talbyte <talbyte@gmail.com>
2016-07-07 17:53:12 +0200

    quiz with result

--- WebContent/js/controller.js
+++ WebContent/js/controller.js
@@ -1,49 +1,53 @@
 angular.module('myApp')
-    .controller('QuizController', ['$scope', '$http', '$route', function($scope, $http, $route) {
+	.controller('QuizController', ['$scope', '$http', '$route', 'values', function($scope, $http, $route, values) {
+	    
+    	$scope.removeQwa = function(questionID) {
+    		$http.delete('http://localhost:8080/luigi-quiz-backend/quiz/qwaservice/' + questionID);
+    		$route.reload();
+    	}
+		
+		$http.get('http://localhost:8080/luigi-quiz-backend/quiz/quizsetservice/').then(function (text) {
+	        $scope.Quiz = text.data;
+	        resetCurrentAnswer();
+	    })
+	    
+	    $scope.currentQuestion = 0;
+	    $scope.irgendwas = values.result;
+	    $scope.nextQuestion = function () {
+	        if (isCorrect()) {
+	            values.result++
+	        }
+	        $scope.currentQuestion++;
+	        resetCurrentAnswer();
+	    };
+	
+	    var resetCurrentAnswer = function () {
+	        $scope.currentAnswer = {};
+	        for (i = 0; i < Object.keys($scope.Quiz.qwaList[$scope.currentQuestion].answers).length; i++) {
+	            $scope.currentAnswer[$scope.Quiz.qwaList[$scope.currentQuestion].answers[i].answerID] = false;
+	        }
+	    }
+	
+	    var isCorrect = function () {
+	        console.log($scope.currentAnswer);
+	        for (i = 0; i < Object.keys($scope.Quiz.qwaList[$scope.currentQuestion].answers).length; i++) {
+	            var answerID = $scope.Quiz.qwaList[$scope.currentQuestion].answers[i].answerID;
+	            console.log($scope.currentAnswer[answerID]);
+	            console.log($scope.Quiz.qwaList[$scope.currentQuestion].answers[i].isCorrect);
+	
+	            if ($scope.currentAnswer[answerID] != $scope.Quiz.qwaList[$scope.currentQuestion].answers[i].isCorrect) {
+	                return false;
+	            }
+	        }
+	        return true;
+	    }
+	}])
+    .controller('QwaController', ['$scope', '$http', '$route', function($scope, $http, $route) {
         
     	$scope.removeQwa = function(questionID) {
     		$http.delete('http://localhost:8080/luigi-quiz-backend/quiz/qwaservice/' + questionID);
     		$route.reload();
     	}
-    	
-        $http.get('http://localhost:8080/luigi-quiz-backend/quiz/quizsetservice').then(function (text) {
-            $scope.Quiz = text.data;
-        })
-        $scope.currentQuestion = 0;
-        $scope.result = 0;
-        $scope.currentAnswer = {};
-        
-
-        $scope.nextQuestion = function () {
-            $scope.currentQuestion++;
-            if (isCurrentAnswerCorrect()){
-                $scope.result++;
-            }
-            $scope.currentAnswer = {};
-            if ($scope.Quiz.qwaList[$scope.currentQuestion]){
-            for (i = 0; i < $scope.Quiz.qwaList[$scope.currentQuestion].answers.length; i++) {
-                var id = $scope.Quiz.qwaList[$scope.currentQuestion].answers[i].answerID;
-                $scope.currentAnswer[id] = false;
-            }}
-        }
-
-        var isCurrentAnswerCorrect = function () {
-            if ($scope.Quiz.qwaList[$scope.currentQuestion]) {
-	            for (i = 0; i < $scope.Quiz.qwaList[$scope.currentQuestion].answers.length; i++) {
-	                if ($scope.Quiz.qwaList[$scope.currentQuestion].answers[i].isCorrect !== $scope.currentAnswer[$scope.Quiz.qwaList[$scope.currentQuestion].answers[i].answerID]) {
-	                    console.log('hier');
-	                    return false;
-	                }
-	            }
-	            return true;
-            } 
-        }
-    }])
-    .controller('QwaController', ['$scope', '$http', function($scope, $http) {
-        
-    	$scope.removeQwa = function(questionID) {
-    		$http.delete('http://localhost:8080/luigi-quiz-backend/quiz/qwaservice/' + questionID);
-        }
     	
     	$scope.editQwa = function(qwa) {
     		$http.put('http://localhost:8080/luigi-quiz-backend/quiz/qwaservice/' + qwa);
---
+++ WebContent/js/value.js
@@ -0,0 +1,2 @@
+angular.module('myApp')
+    .value('values', {result: 0});
